c26e73fc4a89c9394a9d23656a463c65eca98877
Kevin Hung <khung@luc.edu>
2015-09-16 20:32:55 -0500

    updated functions with working break controls

--- ParseData.py
+++ ParseData.py
@@ -5,7 +5,7 @@ from collections import Counter
 def main():
     print('============================================================================')
     print('\n')
-    print('     This  program allows you to see and explore a data set of Chicago.')
+    print('  This program parses and sorts recorded data of issued business licenses.  ')
     print('\n')
     print('============================================================================')
     print('\n')
@@ -15,13 +15,12 @@ def main():
 #GUI
 def options():
     print('\n===== Options =====\n')
-    print('1. Display stats for most common zipcodes.\n')
-    print('2. Display stats for most common wards.\n')
-    print('3. Display stats for most common Police Districts.\n')
-    print('4. Display stats for most common application types.\n')
-    print('5. Show a List of all Fire Stations.\n')
-    print('6. Access function that counts requests within a certain day.\n')
-    x = int(input('Please enter number of option or 0 to exit:'))
+    print('1. Display data for most common zipcodes.\n')
+    print('2. Display data for most common wards.\n')
+    print('3. Display data for most common Police Districts.\n')
+    print('4. Display data for most common application types.\n')
+    print('5. Display data for most common license issue dates.\n')
+    x = int(input('Please enter number of option or 0 to exit: '))
     if x == 1:
         ZipCount()
     elif x == 2:
@@ -31,14 +30,12 @@ def options():
     elif x == 4:
         ApplicationTypes()
     elif x == 5:
-        StationList()
-    elif x == 6:
-        dayData()
+        LicenseIssuedDate()
     elif x == 0:
         exit()
     else:
-        os.system('clear')
         print('That is not a valid option. \nPlease enter a valid number.')
+        raw_input('\nPress any key to return to Options.')
         options()
 
 def ZipCount():
@@ -49,14 +46,20 @@ def ZipCount():
         reader = csv.reader(f)
         for row in reader:
             if row[7] == 'CHICAGO':
-                zipcodes.append(row[9])
+                if row[9] != '':
+                    zipcodes.append(row[9])
         del zipcodes[0]
     zipCounts = Counter(zipcodes)
-    x = input('Please enter from 1 to 10 your desired most common results, or press enter for all')
+    x = input('Please enter your desired amount of top results or 0 for all data: ')
     print('Most common Zip Codes:')
-    for zips, count in zipCounts.most_common(x):
-        print ('%s: %d times.' % (zips, count))
-    input('\nPress any key to return to Options.')
+    if x == 0:
+        for zips, count in zipCounts.most_common():
+            print ('%s: %d times.' % (zips, count))
+    else:
+        for zips, count in zipCounts.most_common(x):
+            print ('%s: %d times.' % (zips, count))
+    raw_input('\nPress any key to return to Options.')
+    os.system('clear')
     options()
 
 
@@ -68,13 +71,21 @@ def WardCount():
         reader = csv.reader(f)
         for row in reader:
             if row[7] == 'CHICAGO':
-                wards.append(row[10])
+                if row[10] != '':
+                    wards.append(row[10])
         del wards[0]
     wardCounts = Counter(wards)
-    x = input('Please enter from 1 to 10 your desired most common results, or press enter for all')
+    x = input('Please enter your desired amount of top results or 0 for all data: ')
     print('Most common Wards:')
-    for ward, count in wardCounts.most_common(x):
-        print ('%s: %d times.' % (ward, count))
+    if x == 0:
+        for ward, count in zipCounts.most_common():
+            print ('%s: %d times.' % (ward, count))
+    else:
+        for ward, count in wardCounts.most_common(x):
+            print ('Ward#%s: %d times.' % (ward, count))
+    raw_input('\nPress any key to return to Options.')
+    os.system('clear')
+    options()
 
 
 def PoliceDistricts():
@@ -85,13 +96,21 @@ def PoliceDistricts():
         reader = csv.reader(f)
         for row in reader:
             if row[7] == 'CHICAGO':
-                districts.append(row[12])
+                if row[12] != '':
+                    districts.append(row[12])
         del districts[0]
     districtCount = Counter(districts)
-    x = input('Please enter from 1 to 10 your desired most common results, or press enter for all')
+    x = input('Please enter your desired amount of top results or 0 for all data: ')
     print('Most common Police Districts:')
-    for district, count in districtsCount.most_common(x):
-        print ('%s: %d times.' % (district, count))
+    if x == 0:
+        for district, count in zipCounts.most_common():
+            print ('%s: %d times.' % (district, count))
+    else:
+        for district, count in districtCount.most_common(x):
+            print ('%s: %d times.' % (district, count))
+    raw_input('\nPress any key to return to Options.')
+    os.system('clear')
+    options()
 
 
 def ApplicationTypes():
@@ -102,15 +121,23 @@ def ApplicationTypes():
         reader = csv.reader(f)
         for row in reader:
             if row[7] == 'CHICAGO':
-                types.append(row[14])
+                if row[14] != '':
+                    types.append(row[14])
         del types[0]
     typeCounts = Counter(types)
-    x = input('Please enter from 1 to 10 your desired most common results, or press enter for all')
+    x = input('Please enter your desired amount of top results or 0 for all data: ')
     print('Most common Application Types:')
-    for type, count in typeCounts.most_common(x):
-        print ('%s: %d times.' % (type, count))
+    if x == 0:
+        for type, count in zipCounts.most_common():
+            print ('%s: %d times.' % (type, count))
+    else:
+        for type, count in typeCounts.most_common(x):
+            print ('%s: %d times.' % (type, count))
+    raw_input('\nPress any key to return to Options.')
+    os.system('clear')
+    options()
 
-def dayData():
+def LicenseIssuedDate():
     os.system('clear')
     print('Starts for date of most licenses issued.')
     dates=[]
@@ -118,12 +145,20 @@ def dayData():
         reader = csv.reader(f)
         for row in reader:
             if row[7] == 'CHICAGO':
-                dates.append(row[24])
+                if row[24] != '':
+                    dates.append(row[24])
         del dates[0]
     dateCounts = Counter(dates)
-    x = input('Please enter from 1 to 10 your desired top results, or press enter for all')
+    x = input('enter your desired amount of top results or 0 for all data: ')
     print('Date of most licenses issued:')
-    for date, count in dateCounts.most_common(x):
-        print ('%s: %d times.' % (date, count))
+    if x == 0:
+        for date, count in zipCounts.most_common():
+            print ('%s: %d times.' % (date, count))
+    else:
+        for date, count in dateCounts.most_common(x):
+            print ('%s: %d times.' % (date, count))
+    raw_input('\nPress any key to return to Options.')
+    os.system('clear')
+    options()
 
 main()
